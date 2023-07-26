f061b2a0c12edf9ab72e3eb26f891d060f28e185
Itatelino de Oliveira Leite Neto <itatelino.oliveira@gmail.com>
2023-07-26 03:02:42 -0300

    Commit01

---
+++ servidornodejs/requisicao.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((requisicao,resposta)=>{
+    resposta.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+    resposta.write('Meu servidor ta rodando...');
+    resposta.end();
+}).listen(1337);
